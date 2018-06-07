b210d49a55a4358bbd3ab4d1169c763de2dabbf3
Gustavo Rodrigues <gugaaroodrigues@gmail.com>
2018-06-07 11:27:07 -0300

    incializado

---
+++ ListaExercicioArrayLis/src/Exercicio4.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
+
+/**
+
+ * @author Gustavo
+ */
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+         ArrayList<Integer> numeros = new ArrayList<> ();
+         for (int i = 0; i < 10; i++){
+             numeros.add(i);
+                
+         } 
+         
+         System.out.println( "Numeros: "
+             + numeros.get(i));
+    }
+
+}
